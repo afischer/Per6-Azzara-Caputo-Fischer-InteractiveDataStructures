f6bd396edd755e7dcd4ca53316037908de21a3cb
Andrew Fischer <afischer15@mac.com>
2014-05-20 17:03:20 -0400

    add(SS) added structure sellector for future updates

---
+++ src/StructureSellector.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class StructureSellector {
+    //erm... do we want to make a gui?
+
+    public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Which data structure would you like to play with?");
+	System.out.println("  1. Bianary Search Trees");
+	
+	choice = reader.next();
+	
+	System.out.println("...This is where we would launch you into our beaautiful visualizer... IF WE HAD ONE!")
+    }
+
+}
