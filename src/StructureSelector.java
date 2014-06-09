511cae35f4349ad9205c283394b185480ed050dd
vazzara <vazzara1@gmail.com>
2014-06-08 21:18:14 -0400

    fixed andrew's spelling errors omg

--- src/StructureSelector.java
+++ src/StructureSelector.java
@@ -10,31 +10,24 @@ public class StructureSelector {
     public static void main (String[] args){  
 	JFrame frame = new JFrame();
 	Object[] possibilities = {"Bianary Search Tree", "Double Linked List", "Graph", "Heap"};
-	String s = (String)JOptionPane.showInputDialog(
-						       frame,
-						       "Choose a data structure!:\n"
-						       ,
-						       "Structure Selector",
-						       JOptionPane.PLAIN_MESSAGE,
-						       null,
-						       possibilities,
-						       "sellect one...");
+	String s = (String)JOptionPane.showInputDialog(frame,"Choose a data structure!:\n", "Structure Selector",
+						       JOptionPane.PLAIN_MESSAGE, null, possibilities,"select one...");
 
 	//If a string was returned, say so.
-	if ((s == "Bianary Search Tree") && (s.length() > 0)) {
-	    /*DO SOEMTHING*/
+	if ((s == "Binary Search Tree") && (s.length() > 0)) {
+	    /*DO SOMETHING*/
 	    return;
 	}
 	if ((s == "Double Linked List") && (s.length() > 0)) {
-	    /*DO SOEMTHING*/
+	    /*DO SOMETHING*/
 	    return;
 	}
 	if ((s == "Graph") && (s.length() > 0)) {
-	    /*DO SOEMTHING*/
+	    /*DO SOMETHING*/
 	    return;
 	}
 	if ((s == "Heap") && (s.length() > 0)) {
-	    /*DO SOEMTHING*/
+	    /*DO SOMETHING*/
 	    return;
 	} else {return;}
     
